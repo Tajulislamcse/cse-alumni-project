4682db71bbcf85be3368a1aa1a1b40d41159423d
tajulislam <tajulislam8028@gmail.com>
2020-05-29 17:53:08 +0600

    simply done modal with committee members

--- cse-alumni/resources/views/committee/index.blade.php
+++ cse-alumni/resources/views/committee/index.blade.php
@@ -5,7 +5,6 @@
 p{background-color:#2C3E50;text-align: center; color:white;padding:10px;margin-top: 25px;font-size: 19px;}
 img{width:100px;height: 100px}
 h2{text-align: center;}
-
 .card {
     background: none repeat scroll 0 0 #FFFFFF;
     border: 1px solid #CCCCCC;
@@ -25,9 +24,6 @@ h2{text-align: center;}
     padding-left: 0px;
     padding-right: 0px;
 }
-
-
-
 .faculty-name-fonts {
     color: #0077B5;
     font-size: 14px;
@@ -35,6 +31,9 @@ h2{text-align: center;}
     line-height: 24px;
     text-decoration: none;
     font-family: Arial, Helvetica, sans-serif;
+}
+.faculty-pic {
+    margin-left: -15px;
 }
 .faculty_Modals {
     cursor: pointer;
@@ -45,7 +44,6 @@ h2{text-align: center;}
     color: #333333;
     font-family: Arial, Helvetica, sans-serif;
 }
-
 .modal-header {
     padding: 15px;
     border-bottom: 1px solid #e5e5e5;
@@ -69,7 +67,6 @@ margin-left: auto;
 order: 2;
 }
 .close {
-
     
     font-size: 21px;
     font-weight: bold;
@@ -77,49 +74,43 @@ order: 2;
     color: black;
     text-shadow: 0 1px 0 white;
     opacity: 0.2;
-
 }
-
 .modal-footer {
     margin-top: 20px;
     padding: 19px 20px 20px;
     text-align: right;
     border-top: 1px solid #e5e5e5;
 }
-
 .modal-body {
     position: relative;
     padding: 20px;
 }
-
 .margin-bottom-10 {
     margin-bottom: 10px;
 }
-
 .btn-default {
     color:white;
     background-color: #3276B1;
     border-color: #cccccc;
 }
-
 </style>
 <div class="container-fluid" style="background-color:#F8F8F8;margin-top:25px">
     <div class="row">
          <div class="col-md-12">
             <h3 style="background-color:pink;text-align: center; color:black;padding:5px">Alumni Association Committee Members</h3>
-             
          </div>
-        
     </div>
 
                <div class="row">
                  <div class="col-md-12">
                     <p>President</p>
                  </div>
-             </div>
+               </div>
            
-            @php if(count($getPresident)==null) echo" <h2>There is no record exist for president</h2>"@endphp
-         
+            @if(count($getPresident)==null)  <h2>There is no record exist for president</h2>
+            
+            @else
+
              <div class="row">
                 @foreach($getPresident as $p)
 
@@ -293,84 +284,51 @@ order: 2;
                     </div>
 
                 </div>
-
-
-
-
-
-
-                    
-
-        
-
-
-
-
-
-
-                            
-
-
-
-
-
-
-             
-             
-    
+@endif
 
 
                <div class="row">
-               <div class="col-md-12">
-                  <p>Secretary</p>
-                </div>
-            </div>
-            
-             @php if(count($getSecretary)==null) echo" <h2>There is no record exist for secretary</h2>"@endphp
+                 <div class="col-md-12">
+                    <p>Secretary</p>
+                 </div>
+             </div>
            
-           <div class="row">
-            @foreach($getSecretary as $s)
-              
+             @if(count($getSecretary)==null) <h2>There is no record exist for Secretary</h2>
+             
 
-               
-         </div>
-     
-                  <div class="row">
-                     <div class="col-md-12">
-                        <p>Members</p>
-                      </div>
-                    </div>
-               @php if(count($getMember)==null) echo" <h2>There is no record exist for member</h2>"@endphp    
-           <div class="row">
-                     @foreach($getMember as $committees)
+             @else       
 
-  
-                        
-                <div class="col-md-3 padding_program ">
+        <div class="row">
+                @foreach($getSecretary as $s)
 
-                    <a class="faculty_Modals" data-toggle="modal" data-target="#myModal17">
+                   
 
-                        </a><div class="row card"><a class="faculty_Modals" data-toggle="modal" data-target="#myModal17">
+
+                <div class="col-md-4 padding_program ">
+
+                    <a class="faculty_Modals" data-toggle="modal" data-target="#myModal2">
+
+                        <div class="row card">
 
                             <div class="col-md-4 faculty-pic">
 
-                                <img src="{{asset('/uploads/'.$committees->image)}}" alt="presidents image">  
+                                <img src="{{asset('/uploads/'.$s->image)}}"alt="">  
 
                             </div>
 
-                            </a><div class="col-md-8 padding_bottom_program"><a class="faculty_Modals" data-toggle="modal" data-target="#myModal17">
+                            <div class="col-md-8 padding_bottom_program">
 
 
 
                                 <div class="col-md-12">
 
-                                    <span class="faculty-name-fonts">{{$committees->name}}</span>
+                                    <span class="faculty-name-fonts">{{$s->name}}</span>
 
                                 </div>
 
-                                </a><div class="col-md-12" style="padding-top:10px;"><a class="faculty_Modals" data-toggle="modal" data-target="#myModal17">
+                                <div class="col-md-12" style="padding-top:10px;">
 
-                                   <i class="fa fa-envelope"></i>&nbsp; </a><a href="mailto:tajulislam8028@gmail.com">{{$committees->email}}</a>
+                                   <i class="fa fa-envelope"></i>&nbsp; <a href="mailto:{{$s->email}}">{{$s->email}}</a>
 
                                 </div>
 
@@ -384,23 +342,23 @@ order: 2;
 
                         </div>
 
-                    
+                    </a>
 
                 </div>
 
 
+                @endforeach
 
-                    @endforeach
             </div>
 
 
 
 
 
+             <!--modal-->
 
-                <!-- Modal -->
 
-                <div class="modal fade" id="myModal17" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 
                     <div class="modal-dialog">
 
@@ -410,7 +368,7 @@ order: 2;
 
                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
 
-                                <h4 class="modal-title model-own" id="myModalLabel">Profile of &nbsp;&nbsp; <span class="faculty_title">Md.Kamal Hossain Chowdhury</span></h4>
+                                <h4 class="modal-title model-own" id="myModalLabel">Profile of &nbsp;&nbsp; <span class="faculty_title">{{$s->name}}</span></h4>
 
                             </div>
 
@@ -420,7 +378,7 @@ order: 2;
 
                                     <div class="col-md-5 col-md-offset-4 margin-bottom-10">
 
-                                        <img src="https://www.cou.ac.bd/content/image/Kamal0000.jpg" alt="" width="100" height="100">  
+                                        <img src="{{asset('/uploads/'.$s->image)}}"alt=""> 
 
                                     </div>
 
@@ -437,50 +395,67 @@ order: 2;
 
                                     <div class="col-md-8">
 
-                                        Chairman of the Department, Computer Science and Engineering
+                                       {{$s->designation}}
                                     </div>
 
                                 </div>
 
+                                <div class="row">
 
+                                    <div class="col-md-4">
+
+                                        Profession :
+
+                                    </div>
+
+                                    <div class="col-md-8">
+                                        
+                                         {{$s->profession}}
+                                    </div>
+
+                                </div>
 
                                 
-
-
-
-
-
+                            
 
                                 <div class="row">
 
                                     <div class="col-md-4">
 
-                                        E-mail :
+                                        Batch :
 
                                     </div>
 
                                     <div class="col-md-8">
-
-                                        <a href="mailto:kamal.cou@gmail.com">kamal.cou@gmail.com</a>
-
-                                    </div>
-
-                                </div>
-
-                                <div class="row">
-
-                                    <div class="col-md-4">
-
-                                        Cell phone :
-
-                                    </div>
-
-                                    <div class="col-md-8">
-
                                         
+                                         {{$s->batch}}
                                     </div>
 
                                 </div>
+
+
+                            
+                                <div class="row">
+
+                                    <div class="col-md-4">
+
+                                        BloodGroup :
+
+                                    </div>
+
+                                    <div class="col-md-8">
+                                        
+                                         {{$s->bloodgroup}}
+                                    </div>
+
+                                </div>
+
+
+
+
+
+
+
 
 
 
@@ -492,7 +467,211 @@ order: 2;
 
                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 
-                                <a href="https://www.cou.ac.bd/cse/Md_Kamal_Hossain_Chowdhury" class="btn btn-primary" type="button">Details</a>
+                            
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+    
+
+
+
+
+@endif
+
+
+                            
+
+
+
+
+
+
+             
+             
+    
+
+
+        
+
+               
+         
+     
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>Members</p>
+                      </div>
+                    </div>
+                  @if(count($getMember)==null)  <h2>There is no record exist for members</h2>
+                  @else    
+                 <div class="row">
+                     @foreach($getMember as $m)
+
+  
+                
+                <div class="col-md-4 padding_program ">
+
+                    <a class="faculty_Modals" data-toggle="modal" data-target="#myModal-{{$m->id}}">
+
+                        <div class="row card">
+
+                            <div class="col-md-4 faculty-pic">
+
+                                <img src="{{asset('/uploads/'.$m->image)}}"alt="">  
+
+                            </div>
+
+                            <div class="col-md-8 padding_bottom_program">
+
+
+
+                                <div class="col-md-12">
+
+                                    <span class="faculty-name-fonts">{{$m->name}}</span>
+
+                                </div>
+
+                                <div class="col-md-12" style="padding-top:10px;">
+
+                                   <i class="fa fa-envelope"></i>&nbsp; <a href="mailto:{{$m->email}}">{{$m->email}}</a>
+
+                                </div>
+
+
+
+
+
+
+
+                            </div>
+
+                        </div>
+
+                    </a>
+
+                </div>
+           
+
+
+
+
+                <!--modal-->
+
+
+                <div class="modal fade" id="myModal-{{$m->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
+                    <div class="modal-dialog">
+
+                        <div class="modal-content">
+
+                            <div class="modal-header">
+
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+
+                                <h4 class="modal-title model-own" id="myModalLabel">Profile of &nbsp;&nbsp; <span class="faculty_title">{{$m->name}}</span></h4>
+
+                            </div>
+
+                            <div class="modal-body">
+
+                                <div class="row">
+
+                                    <div class="col-md-5 col-md-offset-4 margin-bottom-10">
+
+                                        <img src="{{asset('/uploads/'.$m->image)}}"alt=""> 
+
+                                    </div>
+
+                                </div>
+
+                                
+                                <div class="row">
+
+                                    <div class="col-md-4">
+
+                                        Designation :   
+
+                                    </div>
+
+                                    <div class="col-md-8">
+
+                                       {{$m->designation}}
+                                    </div>
+
+                                </div>
+
+                                <div class="row">
+
+                                    <div class="col-md-4">
+
+                                        Profession :
+
+                                    </div>
+
+                                    <div class="col-md-8">
+                                        
+                                         {{$m->profession}}
+                                    </div>
+
+                                </div>
+
+                                
+                            
+
+                                <div class="row">
+
+                                    <div class="col-md-4">
+
+                                        Batch :
+
+                                    </div>
+
+                                    <div class="col-md-8">
+                                        
+                                         {{$m->batch}}
+                                    </div>
+
+                                </div>
+
+
+                            
+                                <div class="row">
+
+                                    <div class="col-md-4">
+
+                                        BloodGroup :
+
+                                    </div>
+
+                                    <div class="col-md-8">
+                                        
+                                         {{$m->bloodgroup}}
+                                    </div>
+
+                                </div>
+
+
+
+
+
+
+
+
+
+
+                            </div>
+
+
+
+                            <div class="modal-footer">
+
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+                            
 
                             </div>
 
@@ -502,9 +681,11 @@ order: 2;
 
                 </div>
 
+          @endforeach
+            </div>
 
 
-
+@endif
 
 
   
@@ -521,6 +702,3 @@ order: 2;
 
 
 @endsection
-
-
-
